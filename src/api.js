353390aa1f4787ec5371293c1a609ee1811fd2b8
igwebdeveloper83 <guzikov.igor83@gmail.com>
2023-07-20 13:04:56 +0200

    check the URL to skip the token parameter addition

--- src/api.js
+++ src/api.js
@@ -2,9 +2,12 @@ import axios from 'axios'
 import { useAuthStore } from './stores/auth'
 
 axios.interceptors.request.use((config) => {
-  const authStore = useAuthStore()
-  let params = new URLSearchParams()
-  params.append('auth', authStore.userInfo.token)
-  config.params = params
+  const url = config.url
+  if (!url.includes('signInWithPassword') && !url.includes('signUp')) {
+    const authStore = useAuthStore()
+    let params = new URLSearchParams()
+    params.append('auth', authStore.userInfo.token)
+    config.params = params
+  }
   return config
 })
